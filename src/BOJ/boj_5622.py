8a64f82c52856ecf231802cc6aaf7dc3f5650b7d
dswook <leewook94@naver.com>
2020-09-27 11:23:41 +0900

    다이얼 문자열 문제

--- src/BOJ/boj_5622.py
+++ src/BOJ/boj_5622.py
@@ -1,8 +1,8 @@
 # 다이얼 문제
-phone = input()
+import sys
+phone = sys.stdin.readline().strip()
 alpabets = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 numbers = {a: num + 2 for num, alpha in enumerate(alpabets) for a in alpha}
 phone_list = [numbers[p] for p in phone]
 phone_time = sum(phone_list) + len(phone_list)
 print(phone_time)
-`
