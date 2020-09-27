ef12f74176ed585052004d9b4a3eb893eb3a2963
dswook <leewook94@naver.com>
2020-09-27 11:38:44 +0900

    type hinting 다이얼 문제

--- src/BOJ/boj_5622.py
+++ src/BOJ/boj_5622.py
@@ -1,8 +1,15 @@
 # 다이얼 문제
 import sys
+from typing import Dict
+
+
+def phone_time(numbers: Dict[str, int]) -> int:
+    phone_list = [numbers[p] for p in phone]
+    return sum(phone_list) + len(phone_list)
+
+
 phone = sys.stdin.readline().strip()
 alpabets = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 numbers = {a: num + 2 for num, alpha in enumerate(alpabets) for a in alpha}
-phone_list = [numbers[p] for p in phone]
-phone_time = sum(phone_list) + len(phone_list)
-print(phone_time)
+
+print(phone_time(numbers))
